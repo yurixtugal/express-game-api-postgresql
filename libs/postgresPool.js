c3866df85c8fcbb2c743362a8a52a95bc4ddf664
yurixtugal <yurixtugal12@gmail.com>
2022-06-23 23:28:25 -0500

    fix ssl error

--- libs/postgresPool.js
+++ libs/postgresPool.js
@@ -4,16 +4,22 @@ const {config} = require('../config/config')
 let URI;
 let USER;
 let PASSWORD;
+let options = {}
 if (config.isProduction){
     URI = config.dbUrl;
+    options.connectionString = config.dbUrl;
+    options.ssl = {
+        rejectUnauthorized: false
+    }
 }else{
     USER = encodeURIComponent(config.dbUser);
     PASSWORD = encodeURIComponent(config.dbPassword);
     URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
+    options.connectionString: URI,
 }
 
 
 
-const pool = new Pool({connectionString: URI});
+const pool = new Pool(options);
 
 module.exports = pool;
