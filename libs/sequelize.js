a3b0f75869c11f0004707e49a7a6caaec406be9a
yurixtugal <yurixtugal12@gmail.com>
2022-06-25 14:34:27 -0500

    Change db configuration

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -20,6 +20,8 @@ if (config.isProduction){
 
 const sequelize = new Sequelize(config.dbUrl,{dialect: 'postgres', logging: true,option}); // Example for postgres
 
+throw(new Error(sequelize));
+
 setUpModels(sequelize);
 
 //sequelize.sync();
